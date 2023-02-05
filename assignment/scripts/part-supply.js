e5b860072c5599688699a57932f2c85d8104be6a
Siv Earley <searley96@gmail.com>
2023-02-05 17:20:27 -0600

    added comment to html and tried last stretch prob

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -71,3 +71,10 @@ console.log(sum);
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop');
+index = 0
+max = 572
+while (index < max) {
+    
+    console.log(index);
+    index++
+}
